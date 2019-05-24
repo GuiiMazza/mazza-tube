90cc25a81597ac6c9e7e74adac31f6b9e4852359
Guilherme Mazza <guiga.mazza@gmail.com>
2019-05-24 17:03:10 -0300

    Reducers and store

--- src/componentes/SearcheBar.js
+++ src/componentes/SearcheBar.js
@@ -8,30 +8,29 @@ import {buscaVideo} from '../store/actions/busca-video';
 
 class SearchBar extends Component {
 
-
-    pesquisaTermo = e => {
-        if (e.keyCode === 13 ) {
-            const termo = e.target.value
-            console.log(termo)
-            this.props.buscaVideo(termo)
-        }
+  pesquisaTermo = e => {
+    if (e.keyCode === 13 ) {
+      const termo = e.target.value
+      console.log(termo)
+      this.props.buscaVideo(termo)
     }
-    render() {
-        return (
-            <div className='search-bar'>
-                <Segment stacked>
-                    <Input icon='search' onKeyDown={(e) => this.pesquisaTermo(e)} size='large' placeholder='Search...' />
-                </Segment>
-            </div>
-        )
-    }
+  }
+  render() {
+    return (
+      <div className='search-bar'>
+        <Segment stacked>
+          <Input icon='search' onKeyDown={this.pesquisaTermo} size='large' placeholder='Search...' />
+        </Segment>
+      </div>
+    )
+  }
    
 }
 
 const mapDispatchProps = (dispatch) => {
-    return {
-        buscaVideo: (termo) => dispatch(buscaVideo(termo))
-    }
+  return {
+    buscaVideo: (termo) => dispatch(buscaVideo(termo))
+  }
 }
 
 export default connect(null, mapDispatchProps)(SearchBar)
